845b61183dd776b4148fff837ac4f02dde2a2b3e
Sandu Victor <vyctor661@gmail.com>
2022-01-05 17:19:10 +0200

    feat: basic bot connecting and activity setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("test");
+import { Client, Intents } from "discord.js";
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const main = async () => {
+    console.info("🚀 Connecting to discord...");
+    await client.login(process.env.BOT_TOKEN);
+    console.info(`🗝️  Logged in as ${client.user?.tag}`);
+
+    client.user?.setActivity("💊 m!help");
+    console.info("🐱 Bot activity has been set.");
+    console.info("👍 The bot is up and running.");
+};
+
+client.on("error", (err) => console.error(err));
+main();
