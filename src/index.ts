d4ae1913adab78379167e2394794b455580dafb9
Sandu Victor <vyctor661@gmail.com>
2022-03-27 20:06:50 +0300

    chore: simple commands exporting

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
 import { Client, Intents } from "discord.js";
-import commands from "./commands/index";
+// import commands from "./commands/index";
+import { textCommands } from "./commands/index";
+import { contextBuilder } from "./modules/contextBuilder";
+import { logHandler } from "./modules/logHandler";
+import { humanMatcher } from "./modules/matchers/humanMatcher";
+import { tryCommands } from "./modules/tryCommands";
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const setSlashCommands = async () => {
-    console.info("🟡 Setting slash commands...");
-    if (!process.env.TESTING_GUILDID) {
-        await client.application?.commands.set(commands);
-    } else {
-        await (
-            await client.guilds.fetch(process.env.TESTING_GUILDID)
-        ).commands.set([]);
-        await (
-            await client.guilds.fetch(process.env.TESTING_GUILDID)
-        ).commands.set(commands);
-    }
-    console.info("🟢 Slash commands have been set.");
-};
+// const setSlashCommands = async () => {
+//     console.info("🟡 Setting slash commands...");
+//     if (!process.env.TESTING_GUILDID) {
+//         await client.application?.commands.set(commands);
+//     } else {
+//         await (
+//             await client.guilds.fetch(process.env.TESTING_GUILDID)
+//         ).commands.set([]);
+//         await (
+//             await client.guilds.fetch(process.env.TESTING_GUILDID)
+//         ).commands.set(commands);
+//     }
+//     console.info("🟢 Slash commands have been set.");
+// };
 
 const setBotActivity = () => {
     client.user?.setActivity("💊 m!help");
@@ -29,10 +34,15 @@ const main = async () => {
     console.info(`🟢 Logged in as ${client.user?.tag}`);
 
     setBotActivity();
-    setSlashCommands();
+    // setSlashCommands();
 
     console.info("🟢 The bot is up and running.");
 };
+
+client.on("messageCreate", (message) => {
+    const context = contextBuilder(client, message);
+    tryCommands(context, [humanMatcher], textCommands, [logHandler]);
+});
 
 client.on("interactionCreate", async (interaction) => {
     // Slash commands test
