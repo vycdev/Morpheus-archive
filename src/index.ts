ac0f5094614280f54af88afb910aedb800945344
Sandu Victor <vyctor661@gmail.com>
2022-08-23 14:35:53 +0300

    chore: removed everything that had to do with slash commands as I will be working on them later
    for now I want to do the simple text commands

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,27 @@
-import { Client, Intents, Interaction } from "discord.js";
-import { SlashCommandBuilder } from "@discordjs/builders";
+import { Client, Intents } from "discord.js";
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_BANS] });
 
-const commands = [
-    new SlashCommandBuilder().setName("ping").setDescription("test the ping")
-].map((command) => command.toJSON());
+const setBotActivity = () => {
+    client.user?.setActivity("💊 m!help");
+    console.info("🟢 Bot activity has been set.");
+};
 
 const main = async () => {
     console.info("🟡 Connecting to discord...");
     await client.login(process.env.BOT_TOKEN);
     console.info(`🟢 Logged in as ${client.user?.tag}`);
 
-    client.user?.setActivity("💊 m!help");
-    console.info("🟢 Bot activity has been set.");
-
-    console.info("🟡 Setting slash commands...");
-    if (!process.env.TESTING_GUILDID) {
-        await client.application?.commands.set(commands);
-    } else {
-        await (
-            await client.guilds.fetch(process.env.TESTING_GUILDID)
-        ).commands.set([]);
-        await (
-            await client.guilds.fetch(process.env.TESTING_GUILDID)
-        ).commands.set(commands);
-    }
-    console.info("🟢 Slash commands have been set.");
+    setBotActivity();
 
     console.info("🟢 The bot is up and running.");
 };
 
-client.on("interactionCreate", async (interaction) => {
-    // Slash commands test
-    if (!interaction.isCommand()) return;
+client.on("messageCreate", async (message) => {
+    if (!message.author.bot) {
 
-    // const { commandName } = interaction;
-
-    interaction.reply("pong");
+        message.channel.send("K");
+    }
 });
 
 client.on("error", (err) => console.error(`🔴 ${err}`));
