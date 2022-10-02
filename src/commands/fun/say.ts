cb90fe702fe3154c98fada9fc5681f342835d4d5
Victor Sandu <vyctor661@gmail.com>
2022-10-02 16:26:16 +0300

    chore: wrote fun with a capital letter in the cateogry

--- src/commands/fun/say.ts
+++ src/commands/fun/say.ts
@@ -6,7 +6,7 @@ export const sayCommand: Command = (context) => [
         name: "Say",
         description: "The bot will repeat what you said.",
         usage: "m!say",
-        category: "fun",
+        category: "Fun",
         cooldown: 500
     },
     [() => prefixMatcher(context, "say")],
