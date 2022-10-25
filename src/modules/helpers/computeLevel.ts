86c8e95a3f006f0f439491c48edfbc80f78ef8a7
Victor-Iulian Sandu <vyctor661@gmail.com>
2022-10-25 20:18:37 +0300

    feat: a bunch of functions to compute levels and xp and get levels from the database

---
+++ src/modules/helpers/computeLevel.ts
@@ -0,0 +1,57 @@
+import { UsersXp } from "@prisma/client";
+import { prisma } from "../..";
+
+const xpMultiplier = parseInt(process.env.XP_MULTIPLIER || "1");
+const scaler = parseInt(process.env.XP_SCALER || "10");
+
+export const computeLevel = (xp: number) => {
+    return Math.floor(Math.sqrt(xp / scaler));
+};
+
+export const computeXp = (level: number) => level * level * scaler;
+
+export const xpGain = (level: number) =>
+    (Math.floor(Math.random() * 10) + 1 + Math.floor(Math.sqrt(level))) *
+    xpMultiplier;
+
+export const totalXpGuild = async (userxp: UsersXp) => {
+    const xpDays = await prisma.xpDays.findMany({
+        where: {
+            usersXpId: userxp.id
+        }
+    });
+    if (!xpDays) return;
+    const totalXp = xpDays
+        .map((v) => v.xp)
+        .reduce((prev, curr) => prev + curr, 0);
+
+    return totalXp;
+};
+
+export const totalXpUser = async (userid: string) => {
+    const usersxp = await prisma.usersXp.findMany({
+        where: {
+            usersUser_id: userid
+        }
+    });
+    if (!usersxp) return;
+
+    const totalXp = (
+        await Promise.all(
+            usersxp.map(async (user) => {
+                const xpDays = await prisma.xpDays.findMany({
+                    where: {
+                        usersXpId: user.id
+                    }
+                });
+                if (!xpDays) return 0;
+                const totalXp = xpDays
+                    .map((v) => v.xp)
+                    .reduce((prev, curr) => prev + curr, 0);
+                return totalXp;
+            })
+        )
+    ).reduce((prev, curr) => prev + curr, 0);
+
+    return totalXp;
+};
